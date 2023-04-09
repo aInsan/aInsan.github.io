ed92eba735f26eba7273d21b742cc4fd27bff409
aInsan <ainsan2006@gmail.com>
2023-04-09 19:31:36 -0400

    JS and CSS

--- script.js
+++ script.js
@@ -1,45 +1,22 @@
-const GITHUB_URL = 'https://api.github.com/users/aInsan/repos?sort=updated&per_page=6';
+const repoList = document.getElementById("repo-list");
 
 async function getRepos() {
-  try {
-    const response = await fetch(GITHUB_URL);
+    const response = await fetch("https://api.github.com/users/aInsan/repos");
     const data = await response.json();
     return data;
-  } catch (error) {
-    console.error(error);
-  }
 }
 
-function createProjectItem(project) {
-  const { name, html_url } = project;
-  const description = project.description || 'No description provided.';
-  const updated = new Date(project.updated_at).toLocaleDateString();
-  const language = project.language || 'N/A';
-
-  const projectItem = `
-    <div class="project-item">
-      <h3>${name}</h3>
-      <p>${description}</p>
-      <p><strong>Language:</strong> ${language}</p>
-      <p><strong>Last updated:</strong> ${updated}</p>
-      <a href="${html_url}" target="_blank" rel="noopener noreferrer">View on GitHub</a>
-    </div>
-  `;
-
-  return projectItem;
+function displayRepos(repos) {
+    repoList.innerHTML = "";
+    repos.forEach(repo => {
+        const li = document.createElement("li");
+        const a = document.createElement("a");
+        a.href = repo.html_url;
+        a.target = "_blank";
+        a.textContent = repo.name;
+        li.appendChild(a);
+        repoList.appendChild(li);
+    });
 }
 
-async function displayProjects() {
-  const projectsContainer = document.querySelector('.project-grid');
-
-  try {
-    const projects = await getRepos();
-    const projectItems = projects.map(project => createProjectItem(project)).join('');
-    projectsContainer.innerHTML = projectItems;
-  } catch (error) {
-    console.error(error);
-    projectsContainer.innerHTML = '<p>Unable to load projects. Please try again later.</p>';
-  }
-}
-
-displayProjects();
+getRepos().then(data => displayRepos(data));
