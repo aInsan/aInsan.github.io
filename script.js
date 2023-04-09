856e2e2adcf20f3fcd217ba9c4ffc20bddea0764
aInsan <ainsan2006@gmail.com>
2023-04-09 19:56:09 -0400

    New

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-const repoList = document.getElementById("repo-list");
+const projectList = document.querySelector("#project-list");
 
-async function getRepos() {
-    const response = await fetch("https://api.github.com/users/aInsan/repos");
-    const data = await response.json();
-    return data;
-}
+const displayProjects = async () => {
+    try {
+        const response = await fetch("https://api.github.com/users/aInsan/repos");
+        const projects = await response.json();
+        projects.forEach((project) => {
+            const projectItem = document.createElement("li");
+            projectItem.classList.add("project-item");
+            projectItem.innerHTML = `
+                <h3>${project.name}</h3>
+                <p>${project.description}</p>
+                <a href="${project.html_url}" target="_blank">View on GitHub</a>
+            `;
+            projectList.appendChild(projectItem);
+        });
+    } catch (error) {
+        console.error(error);
+    }
+};
 
-function displayRepos(repos) {
-    repoList.innerHTML = "";
-    repos.forEach(repo => {
-        const li = document.createElement("li");
-        const a = document.createElement("a");
-        a.href = repo.html_url;
-        a.target = "_blank";
-        a.textContent = repo.name;
-        li.appendChild(a);
-        repoList.appendChild(li);
-    });
-}
-
-getRepos().then(data => displayRepos(data));
+displayProjects();
